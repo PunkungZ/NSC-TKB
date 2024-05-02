2e656979552e59e1f4b1b1f01070bee27654df32
Frame2145555 <155990240+Frame2145555@users.noreply.github.com>
2024-05-02 23:12:18 +0700

    player code

---
+++ Assets/Dev_Scene/F/Code_Player/Base_Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Base_Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    protected bool isGrounded;
+    protected Rigidbody2D rb;
+    protected Transform playerSprite;
+
+    protected virtual void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerSprite = transform.Find("PlayerSprite");
+    }
+
+    protected abstract void HandleMovement();
+
+    protected void Update()
+    {
+        HandleMovement();
+    }
+
+    protected void MovementPlayer(string horizontalInput, string jumpInput)
+    {
+        float move = Input.GetAxis(horizontalInput);
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown(jumpInput) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (move < 0)
+        {
+            playerSprite.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (move > 0)
+        {
+            playerSprite.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    protected void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}
+
---
+++ Assets/Dev_Scene/F/Code_Player/Player1/Player_1_Movement.cs
@@ -0,0 +1,7 @@
+public class Player_1_Movement : Base_Player
+{
+    protected override void HandleMovement()
+    {
+        MovementPlayer("Player1Horizontal", "Player1Jump");
+    }
+}
