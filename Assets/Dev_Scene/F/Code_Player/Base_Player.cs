72eafb5cea0e05c172ee99d1529592b2a1b44177
Frame2145555 <155990240+Frame2145555@users.noreply.github.com>
2024-05-03 15:58:55 +0700

    แก้ code 2

--- Assets/Dev_Scene/F/Code_Player/Base_Player.cs
+++ Assets/Dev_Scene/F/Code_Player/Base_Player.cs
@@ -8,36 +8,56 @@ public class Base_Player : MonoBehaviour
     [SerializeField] private float jumpForce = 10f;
     private bool isGrounded;
     private Rigidbody2D rb;
-    private GameObject playerSprite;
+
+    private GameObject playerSprite1;
+    private GameObject playerSprite2;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerSprite = GameObject.Find("Player1");
     }
 
-    void Update()
+    public void MovementPlayer1()
     {
-        MovementPlayer();
-    }
+        playerSprite1 = GameObject.Find("Player1");
 
-    private void MovementPlayer()
-    {
-        float move = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Horizontal1");
         rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump1") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
         if (move < 0)
         {
-            playerSprite.transform.localScale = new Vector3(-1, 1, 1);
+            playerSprite1.transform.localScale = new Vector3(-1, 1, 1);
         }
         else if (move > 0)
         {
-            playerSprite.transform.localScale = new Vector3(1, 1, 1);
+            playerSprite1.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    public void MovementPlayer2()
+    {
+        playerSprite2 = GameObject.Find("Player2");
+
+        float move = Input.GetAxis("Horizontal2");
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump2") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (move < 0)
+        {
+            playerSprite2.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (move > 0)
+        {
+            playerSprite2.transform.localScale = new Vector3(1, 1, 1);
         }
     }
 
---
+++ Assets/Dev_Scene/F/Code_Player/Player2/Player2_Movement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_Movement : Base_Player
+{
+    void Update()
+    {
+        MovementPlayer2();
+    }
+}
