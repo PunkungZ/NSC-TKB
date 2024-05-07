2b5f3311700b71fd002afce5a2963d90ce712f0d
PunkungZ <punpunpro4@gmail.com>
2024-05-07 21:21:11 +0700

    ...

--- Assets/Dev_Scene/Pun/Scripts/Timer.cs
+++ Assets/Dev_Scene/Pun/Scripts/Timer.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float reminingTime;
-    
+    public GameManagerScript gameManager;
+    public GameObject gameOverUI;
+
+    bool isdead;
+
     void Update()
     {
         if (reminingTime > 0) 
@@ -20,8 +25,21 @@ public class Timer : MonoBehaviour
             // GameOver();
             timerText.color = Color.red;
         }
+
+        if (reminingTime <= 0 && !isdead)
+        {
+            isdead = true;
+            gameManager.gameOver();
+        }
         int minutes = Mathf.FloorToInt(reminingTime / 60);
         int seconds = Mathf.FloorToInt(reminingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}",  minutes, seconds);
     }
+
+    public void gameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+
 }
