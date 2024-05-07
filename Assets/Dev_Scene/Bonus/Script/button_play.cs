b3ebf82cd1ee7c213f8026d69a62879b68eb2fa5
Paloma004 <hvthay0599@gmail.com>
2024-05-07 22:03:55 +0700

    UI

---
+++ Assets/Dev_Scene/Bonus/Script/button_play.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button_play : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }   
+    public void PlayGamesetting()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }   
+    public void PlayGamecreit()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }   
+}
