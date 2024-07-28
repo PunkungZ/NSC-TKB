40970eecfab8183c902152ba756b36d5adf79cb3
Frame2145555 <155990240+Frame2145555@users.noreply.github.com>
2024-07-28 19:14:19 +0700

    ระบบเลือกตัวละคร

--- Assets/Dev_Scene/GameManager.cs
+++ Assets/Dev_Scene/GameManager.cs
@@ -6,14 +6,21 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private Transform player1SpawnPoint;
     [SerializeField] private Transform player2SpawnPoint;
-    [SerializeField] private GameObject[] characterPrefabs;
+    [SerializeField] private GameObject[] player1Prefabs;
+    [SerializeField] private GameObject[] player2Prefabs;
 
     void Start()
     {
         int player1Index = PlayerPrefs.GetInt("Player1Character", 0);
         int player2Index = PlayerPrefs.GetInt("Player2Character", 0);
 
-        Instantiate(characterPrefabs[player1Index], player1SpawnPoint.position, Quaternion.identity);
-        Instantiate(characterPrefabs[player2Index], player2SpawnPoint.position, Quaternion.identity);
+        string player1PrefabName = PlayerPrefs.GetString("Player1Prefab");
+        string player2PrefabName = PlayerPrefs.GetString("Player2Prefab");
+
+        GameObject player1Prefab = System.Array.Find(player1Prefabs, prefab => prefab.name == player1PrefabName);
+        GameObject player2Prefab = System.Array.Find(player2Prefabs, prefab => prefab.name == player2PrefabName);
+
+        Instantiate(player1Prefab, player1SpawnPoint.position, Quaternion.identity);
+        Instantiate(player2Prefab, player2SpawnPoint.position, Quaternion.identity);
     }
 }
